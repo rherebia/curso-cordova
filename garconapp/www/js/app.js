948051192fbe69d020c7c541a0fdc8ec7a256fa1
Renato Herebia <rherebia@gmail.com>
2017-02-09 08:39:27 -0300

    Assistindo aula 8

--- garconapp/www/js/app.js
+++ garconapp/www/js/app.js
@@ -36,3 +36,15 @@ $('.acao-limpar').on('click', function() {
    $('#numero-mesa').val('');
     $('.badge').remove();
 });
+
+$('.scan-qrcode').click(function () {
+    cordova.plugins.barcodeScanner.scan(function (resultado) {
+        if (resultado.text) {
+            Materialize.toast('Mesa ' + resultado.text, 2000);
+
+            $('#numero-mesa').val(resultado.text);
+        }
+    }, function(erro) {
+        Materialize.toast('Erro ' + erro, 2000, 'red-text');
+    });
+});
